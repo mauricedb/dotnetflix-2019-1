e4132ef7567cb45a454567efea6f15706d827d6f
Maurice de Beijer <maurice.de.beijer@gmail.com>
2019-10-05 10:25:14 +0200

    Copy

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, StrictMode } from "react";
+import "./App.css";
+import Clock from "./components/clock";
+// import Jokes from "./components/jokes";
+import Loading from "./components/loading";
+
+import { unstable_scheduleCallback, unstable_NormalPriority } from "scheduler";
+
+const Jokes = React.lazy(() => import("./components/jokes"));
 
 const App: React.FC = () => {
+  const [displayJokes, setDisplayJokes] = React.useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StrictMode>
+      <div>
+        <Suspense fallback={<Loading />}>
+          <Clock />
+          <div>
+            <label>
+              <input
+                type="checkbox"
+                checked={displayJokes}
+                // onChange={() => setDisplayJokes(!displayJokes)}
+                onChange={() => {
+                  unstable_scheduleCallback(unstable_NormalPriority, () =>
+                    setDisplayJokes(!displayJokes)
+                  );
+                }}
+              />
+              Display jokes
+            </label>
+          </div>
+          {displayJokes && <Jokes url="/api/jon-skeet.json" />}
+        </Suspense>
+      </div>
+    </StrictMode>
   );
-}
+};
 
 export default App;
